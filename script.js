d1ab5b6d7645cbeb201a07a3079eb31d4ac70eac
Shriyankidubey <82280960+Shriyankidubey@users.noreply.github.com>
2022-03-06 14:46:16 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+
+let plus = document.querySelector("#plus");
+let imgListContainer = document.querySelector(".img-list-container");
+let imgShowContainer = document.querySelector(".img-show_container");
+let imgsArr = [];
+let ciimgIndex = -1;
+let uniqueIdentifier =0;
+
+
+plus.addEventListener("click", function () {
+    let imgLink = prompt("please enter img url");
+    // img url nahi put kiya hai 
+    if (imgLink == null || imgLink == "") {
+        alert("Please enter img src");
+        return;
+    }
+
+    //    add image to List & add Image to show container
+    addImageToListandShow(imgLink)
+})
+function addImageToListandShow(imgLink) {
+    // img list
+    let previewImg = document.createElement("img");
+    previewImg.setAttribute("src", imgLink);
+    // img 
+    previewImg.setAttribute("cid", uniqueIdentifier);
+    previewImg.setAttribute("class", "img_preview");
+
+    // last me add karta hai 
+    imgListContainer.appendChild(previewImg);
+    //    show image
+    setShowImage(imgLink, uniqueIdentifier)
+    // to enable image chanage on click;
+    handleImagFunctionality(uniqueIdentifier, imgLink, previewImg);
+    uniqueIdentifier++;
+    // console.log(imgsArr);
+    function handleImagFunctionality(uniqueIdentifier, imgLink, previewImg) {
+        let imgIdentifierObj = {
+            cid: uniqueIdentifier,
+            url: imgLink
+        }
+        imgsArr.push(imgIdentifierObj);
+        // images aayi unko click waali feature do
+        previewImg.addEventListener("click", function () {
+            let CImgid = previewImg.getAttribute("cid");
+            //    O(1)
+            let cImgSrc = previewImg.getAttribute("src");
+            setShowImage(cImgSrc);
+    
+            // console.log(cid);
+            // console.log(imgsArr)
+            // O(n);
+            for (let i = 0; i < imgsArr.length; i++) {
+                let imgIdentifierObj = imgsArr[i];
+                if (CImgid == imgIdentifierObj.cid) {
+                    //    wahi url chaiye
+                    console.log(imgIdentifierObj.url);
+                    // put kisme apne 
+                    setShowImage(imgIdentifierObj.url)
+                }
+                
+            }
+        })
+    }
+
+
+    function setShowImage(imgLink){
+    let innerHtmlBlock =
+    ` <span class="material-icons">keyboard_double_arrow_left</span>
+    <img class="final_image" src=${imgLink} alt=""> 
+    <span class="material-icons">keyboard_double_arrow_right</span>`
+    // img show -> replace 
+    imgShowContainer.innerHTML = innerHtmlBlock;
+}
+}
+
